42d9f68d740570a032937c638816cc1f494a24b3
Li Chaoqun <lee.chaoqun@gmail.com>
2017-03-03 22:55:22 +0800

    use json and h5 to save model file and weights file separately

--- train.py
+++ train.py
@@ -1,20 +1,36 @@
 import pandas as pd
+from keras.models import model_from_json
 from sklearn.model_selection import train_test_split
 
 import utils
 
+MODEL_WEIGHTS = 'model.h5'
+MODEL_JSON = 'model.json'
 EPOCHS = 10
 BATCH_SIZE = 64
 
 
+def get_model():
+    try:
+        with open(MODEL_JSON, 'r') as f:
+            model = model_from_json(f.read())
+        model.compile('adam', 'mse')
+        model.load_weights(MODEL_WEIGHTS)
+        print('Load from trained model')
+    except:
+        model = utils.nvidia_model((160, 320, 3), with_cropping=True)
+        print('train new model')
+
+    return model
+
+
 def main():
     track1 = utils.read_driving_log('data')
-    # driving_log = track1
     track2 = utils.read_driving_log('track2', has_header=False)
     driving_log = pd.concat([track1, track2])
     # driving_log = utils.sample(driving_log, 0.1, 1000)
 
-    model = utils.nvidia_model((160, 320, 3), with_cropping=True)
+    model = get_model()
 
     train, validation = train_test_split(driving_log, test_size=0.2)
 
@@ -29,7 +45,10 @@ def main():
                         validation_data=utils.data_generator(validation, BATCH_SIZE, augment=False),
                         nb_val_samples=len(validation) * 3)
 
-    model.save('model.h5')
+    model.save_weights(MODEL_WEIGHTS)
+
+    with open(MODEL_JSON, 'w') as f:
+        f.write(model.to_json())
 
 
 if __name__ == '__main__':
